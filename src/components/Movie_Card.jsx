0ffd93f53f8906ec0c03d45a050ba7e429899aed
sujo030902 <dhksujal03@gmail.com>
2025-02-15 13:54:18 +0530

    fixed scrolling card issue

--- src/components/Movie_Card.jsx
+++ src/components/Movie_Card.jsx
@@ -1,21 +1,23 @@
-import React from 'react'
+import React from 'react';
 
 const Movie_Card = () => {
   return (
-    <>
-        <div className='w-full px-2'>
-            <h1 className='text-white text-3xl font-bold mt-12 text-left w-full'>Trending Movies</h1>
-        </div> 
-        <div className='w-full bg-black h-full px-4 py-6 flex gap-4 overflow-hidden'>
-            <div className='border-2 w-1/2 h-[60%]'>
-            </div>
-            <div className='border-2 w-1/2 h-[60%]'>
-            </div>
-            <div className='border-2 w-1/2 h-[60%]'>
-            </div>
+    <div className="w-full bg-black text-white px-4 py-6">
+      <h1 className="text-3xl font-bold mb-6">Trending Movies</h1>
+
+      {/* Scrollable container */}
+      <div className="w-full overflow-x-auto whitespace-nowrap scrollbar-hide">
+        <div className="flex gap-4">
+          <div className="border-2 h-60 w-40 min-w-[160px] bg-gray-800"></div>
+          <div className="border-2 h-60 w-40 min-w-[160px] bg-gray-800"></div>
+          <div className="border-2 h-60 w-40 min-w-[160px] bg-gray-800"></div>
+          <div className="border-2 h-60 w-40 min-w-[160px] bg-gray-800"></div>
+          <div className="border-2 h-60 w-40 min-w-[160px] bg-gray-800"></div>
+          <div className="border-2 h-60 w-40 min-w-[160px] bg-gray-800"></div>
         </div>
-    </>
-  )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Movie_Card;
