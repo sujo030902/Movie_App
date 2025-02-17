9bed04595fbf5aa0202242491837fe9f4a9b74eb
sujo030902 <dhksujal03@gmail.com>
2025-02-17 17:44:52 +0530

    added only card for future use

--- src/components/Movie_Card.jsx
+++ src/components/Movie_Card.jsx
@@ -2,17 +2,12 @@ import React from 'react';
 
 const Movie_Card = () => {
   return (
-    <div className="w-full bg-black text-white px-4 py-6">
+    <div className="w-full  text-white px-4 py-6">
       <h1 className="text-3xl font-bold mb-6">Trending Movies</h1>
 
       {/* Scrollable container */}
       <div className="w-full overflow-x-auto whitespace-nowrap scrollbar-hide">
         <div className="flex gap-4">
-          <div className="border-2 h-60 w-40 min-w-[160px] bg-gray-800"></div>
-          <div className="border-2 h-60 w-40 min-w-[160px] bg-gray-800"></div>
-          <div className="border-2 h-60 w-40 min-w-[160px] bg-gray-800"></div>
-          <div className="border-2 h-60 w-40 min-w-[160px] bg-gray-800"></div>
-          <div className="border-2 h-60 w-40 min-w-[160px] bg-gray-800"></div>
           <div className="border-2 h-60 w-40 min-w-[160px] bg-gray-800"></div>
         </div>
       </div>
