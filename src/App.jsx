d0d5b9d18bda2460dc4046a794047b06d43d41d7
sujo030902 <dhksujal03@gmail.com>
2025-02-18 22:26:04 +0530

    bg-change

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,8 @@ const App = () => {
 
   return (
     <>
-      <div className='w-screen h-screen flex flex-col justify-center items-center px-2'>
-      <h1 className='text-4xl text-center'>Bookmarks Your <span className='text-purple-400'>Favourite Movies</span> For Binge Watching
+      <div className='w-screen h-screen flex flex-col justify-center items-center px-2 bg-black'>
+      <h1 className='text-4xl text-center text-white'>Bookmarks Your <span className='text-purple-400'>Favourite Movies</span> For Binge Watching
       <Search search = {search} handleSearch = {handleSearch} />
       </h1>
       <h1>{search}</h1>
